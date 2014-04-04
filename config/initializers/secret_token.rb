fd18b403458a3fe16aa0683c5f8ec5c0614cf651
Ben Wong <ben.wong@unboxedconsulting.com>
2014-04-04 16:22:24 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HelloEngineYard::Application.config.secret_key_base = '7b052973f6a3b33a1b13d847941fca446a0d878f5125c89afa9ea66089658f65727d9b7222878d6ae7af2dd14ba8fd130a82788b927b204451f9653906f5273e'
